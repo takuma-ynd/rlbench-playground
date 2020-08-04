4deda09a00d4cd30a043194404b84f5e9ca2d1e7
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-08-04 23:46:10 +0900

    adapt simple_reacher.py to current env

--- simple_reacher.py
+++ simple_reacher.py
@@ -4,41 +4,18 @@ from rlbench.environment import Environment
 from rlbench.action_modes import ArmActionMode, ActionMode
 from rlbench.observation_config import ObservationConfig
 from rlbench.tasks import ReachTarget as Task
+from pfrl_simple_reacher import WristObsWrapper, GraspActionWrapper, NormalizeAction
+from gym.wrappers import ResizeObservation, RescaleAction
+from gym.wrappers.flatten_observation import FlattenObservation
 import numpy as np
 import cv2
 import os
 
-env = gym.make('reach_target-vision-v0', render_mode='human')
+env = NormalizeAction(GraspActionWrapper(FlattenObservation(ResizeObservation(WristObsWrapper(gym.make('reach_target-ee-vision-v0', render_mode='human')), (64, 64))), 3))
+# env = gym.make('reach_target-vision-v0', render_mode='human')
 
-# class Agent(object):
-
-#     def __init__(self, action_size):
-#         self.action_size = action_size
-#         print('action size:', self.action_size)
-
-#     def act(self, obs):
-#         # arm = np.array([float(input('input {}'.format(i))) for i in range(self.action_size - 1)])
-#         arm = np.random.normal(0.0, 0.2, size=(self.action_size - 1,))
-#         gripper = [1.0]  # Always open
-#         return np.concatenate([arm, gripper], axis=-1)
-
-
-# obs_config = ObservationConfig()
-# obs_config.set_all(True)
-
-# action_mode = ActionMode(ArmActionMode.ABS_JOINT_VELOCITY)
-# env = Environment(
-#     action_mode, obs_config=obs_config, headless=False)
-# env.launch()
-
-# task = env.get_task(Task)
-
-# agent = Agent(env.action_size)
-
-
-
-training_steps = 120
-episode_length = 40
+training_steps = 512
+episode_length = 512
 for i in range(training_steps):
     if i % episode_length == 0:
         print('Reset Episode')
@@ -47,11 +24,10 @@ for i in range(training_steps):
     env.render()  # Note: rendering increases step time.
 
     print(reward)
-    path = os.path.join(os.path.dirname(__file__), 'observations' , '{:03}.jpg'.format(i))
-    print(path)
-    wrist_obs = obs['wrist_rgb']
-    img_obs = (wrist_obs * 255).astype(np.uint8)
-    img_obs = cv2.cvtColor(img_obs, cv2.COLOR_BGR2RGB)
-    cv2.imwrite(path, img_obs)
+    # path = os.path.join(os.path.dirname(__file__), 'observations' , '{:03}.jpg'.format(i))
+    # print(path)
+    # img_obs = (wrist_obs * 255).astype(np.uint8)
+    # img_obs = cv2.cvtColor(img_obs, cv2.COLOR_BGR2RGB)
+    # cv2.imwrite(path, img_obs)
 print('done')
 env.close()
